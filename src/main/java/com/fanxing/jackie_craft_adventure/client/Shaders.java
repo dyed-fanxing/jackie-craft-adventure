f2611dd55a86921efc324d6f18f1ff85df3aa68e
dyed_fanxing <1941501914@qq.com>
2026-05-02 20:38:18 +0800

    更新lang

--- src/main/java/com/fanxing/jackie_craft_adventure/client/Shaders.java
+++ src/main/java/com/fanxing/jackie_craft_adventure/client/Shaders.java
@@ -1,16 +1,17 @@
 package com.fanxing.jackie_craft_adventure.client;
 
 import com.fanxing.jackie_craft_adventure.JackieCraftAdventure;
-import net.minecraft.client.renderer.ShaderInstance;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RegisterShadersEvent;
+
+import java.io.IOException;
 
 @EventBusSubscriber(modid = JackieCraftAdventure.MOD_ID, value = Dist.CLIENT)
 public class Shaders {
-    public static ShaderInstance screenShader;
-
-    public static ShaderInstance getScreenShader() {
-        return screenShader;
+    @SubscribeEvent
+    public static void onRegisterShaders(RegisterShadersEvent event) throws IOException {
     }
 
 }
